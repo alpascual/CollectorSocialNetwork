659f34039c86ae9930c0880bce6c4f5e7c10364d
Al <alpascual@gmail.com>
2012-07-24 19:01:05 -0700

    first commit

---
+++ CollectorSocialNetwork/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectorSocialNetwork
+//
+//  Created by Albert Pascual on 7/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectorSocialNetwork/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  CollectorSocialNetwork
+//
+//  Created by Albert Pascual on 7/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
